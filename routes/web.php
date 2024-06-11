019c238b53f73adf13533f2a6381a32fb7b30b80
Dreftrog <diegoguzman1400@gmail.com>
2024-06-11 01:52:04 -0600

    a

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/submit', [FormController::class, 'submitForm'])->name('form.submit');
