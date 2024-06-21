e50ccddb67471e139a7ef64a64fcaecb7b4bbe67
SirAlonne16 <ben.pobletea@duocuc.cl>
2024-06-21 00:09:30 -0400

    Glitch 2.0

--- public/server.php
+++ public/server.php
@@ -11,6 +11,18 @@ $app->run();
 
 define('LARAVEL_START', microtime(true));
 
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+$uri = $uri === '/' ? '/index.php' : $uri;
+
+if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
+    return false;
+}
+
+require_once __DIR__ . '/public/index.php';
+
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
